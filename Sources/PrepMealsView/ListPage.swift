e317b7608b569d3ad750a3e81a9a61ce8563d62c
Ahmed Khalaf <pxlshpr@gmail.com>
2022-11-04 08:44:21 +0500

    bump

--- Sources/PrepMealsView/ListPage.swift
+++ Sources/PrepMealsView/ListPage.swift
@@ -157,7 +157,7 @@ public struct ListPage: View {
     
     var mealTimeSuggestions: [Date] {
         meals.map({Date(timeIntervalSince1970: $0.time)})
-            .suggestedMealTimes(includingSuggestionsRelativeToNow: isToday)
+            .suggestedMealTimes(includingSuggestionsRelativeToNow: isToday, forDate: date)
     }
     
     var addMealButtons: some View {
@@ -259,14 +259,14 @@ struct ListRowBackground: View {
 //MARK: - Prep Specific
 
 extension Array where Element == Date {
-    func suggestedMealTimes(includingSuggestionsRelativeToNow: Bool = false) -> [Date] {
+    func suggestedMealTimes(includingSuggestionsRelativeToNow: Bool = false, forDate dayDate: Date) -> [Date] {
         
         func newDateIfOnSameDay(_ hours: Int, hoursAfter date: Date) -> Date? {
             guard hours > 0 else { return nil }
             let newDate = date.atClosestHour.movingHourBy(hours)
             
             /// If the `newDate` is on the next day, only return it if it's before 6am
-            if newDate.day > date.day  {
+            if !Calendar.autoupdatingCurrent.isDate(newDate, inSameDayAs: dayDate) {
                 guard newDate.h < 6 else { return nil }
             }
             return newDate
@@ -279,11 +279,14 @@ extension Array where Element == Date {
 
         let suggestions: [Date?]
         if includingSuggestionsRelativeToNow {
+            
             /// if the last meal is within 1 hour from now (in either direction)
             let twoHoursFromNow = newDateIfOnSameDay(2, hoursAfter: Date())
             let fourHoursFromNow = newDateIfOnSameDay(4, hoursAfter: Date())
-            //TODO: Check that these are within the wee hours of the next day
-            if abs(last.timeIntervalSince1970 - Date().timeIntervalSince1970) < 3600 {
+            
+            if abs(last.timeIntervalSince1970 - Date().timeIntervalSince1970) < 3600
+                || twoHoursFromLast == nil
+            {
                 suggestions = [twoHoursFromNow, fourHoursFromNow]
             } else {
                 suggestions = [twoHoursFromLast, twoHoursFromNow]
