bfb8990870d7fe50729db9368b25ff2fbbd8cd52
Ahmed Khalaf <pxlshpr@gmail.com>
2023-02-17 15:15:36 +0500

    bump

--- Sources/PrepMealsView/DayView.swift
+++ Sources/PrepMealsView/DayView.swift
@@ -89,8 +89,7 @@ public struct DayView: View {
         
         var metricsView: some View {
             MetricsView(date: $date)
-            .padding(.horizontal, 20)
-            .frame(height: 150)
+                .padding(.horizontal, 10)
 //            .padding(.horizontal, 20)
 //            /// ** Important ** This explicit height on the encompassing `ZStack` is crucial to ensure that
 //            /// the separator heights of the `MealView`'s don't get messed up (it's a wierd bug that's device dependent).
@@ -99,18 +98,28 @@ public struct DayView: View {
             .transition(transition)
         }
         
-        return ScrollView(showsIndicators: false) {
-            metricsView
-            ForEach(Array(dayMeals.enumerated()), id: \.element.id) { (index, item) in
-                mealView(for: $dayMeals[index])
-                    .transition(transition)
+        var scrollView: some View {
+            ScrollView(showsIndicators: false) {
+//                metricsView
+                ForEach(Array(dayMeals.enumerated()), id: \.element.id) { (index, item) in
+                    mealView(for: $dayMeals[index])
+                        .transition(transition)
+                }
+                Color.clear
+                    .frame(height: 1)
+                    .frame(maxWidth: .infinity)
             }
-            Color.clear
-                .frame(height: 1)
-                .frame(maxWidth: .infinity)
+            .safeAreaInset(edge: .bottom) { Color.clear.frame(height: 55) }
+            .scrollContentBackground(.hidden)
+            .background(backgroundLayer)
         }
-        .safeAreaInset(edge: .bottom) { Color.clear.frame(height: 55) }
-        .scrollContentBackground(.hidden)
+        
+//        return scrollView
+        return VStack(spacing: 0) {
+            metricsView
+            Divider()
+            scrollView
+        }
         .background(backgroundLayer)
     }
     
@@ -148,224 +157,5 @@ public struct DayView: View {
             isAnimatingItemChange: $isAnimatingItemChange,
             actionHandler: actionHandler
         )
-    }
-}
-
-//NEXT:
-/// [ ] Use previousDate below to mark the date before it's being transitioned—so that we can use it by the empty state to correctly show content (otherwise it changes to the "mark as fasted" content duringt he transition when moving back from yesterday to today.
-/// [ ] Set showingEmpty initially
-///
-extension DayView {
-    class ViewModel: ObservableObject {
-        
-        @Published var day: Day?
-        @Published var dayMeals: [DayMeal]
-        @Published var markedAsFasted: Bool = false
-        
-        @Published var emptyContentHeight: CGFloat = 0
-
-        @Published var previousDate: Date = Date()
-        @Published var showingEmpty: Bool = false
-
-        @Published var animatingMeal = false
-        
-        var date: Date {
-            didSet {
-                dateChanged(date)
-            }
-        }
-        
-        init(date: Date) {
-            self.date = date
-            self.previousDate = date
-            let day = DataManager.shared.day(for: date)
-            self.day = day
-            self.dayMeals = day?.meals ?? []
-            self.showingEmpty = dayMeals.isEmpty
-            
-            addObservers()
-        }
-    }
-}
-
-extension DayView.ViewModel {
-    
-    func addObservers() {
-        NotificationCenter.default.addObserver(self, selector: #selector(didAddFoodItemToMeal), name: .didAddFoodItemToMeal, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didDeleteFoodItemFromMeal), name: .didDeleteFoodItemFromMeal, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didDeleteMeal), name: .didDeleteMeal, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateMealFoodItem), name: .didUpdateMealFoodItem, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateFoodItems), name: .didUpdateFoodItems, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(didAddMeal), name: .didAddMeal, object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(didUpdateMeal), name: .didUpdateMeal, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(didSetBadgeWidths), name: .didSetBadgeWidths, object: nil)
-        
-        NotificationCenter.default.addObserver(self, selector: #selector(initialSyncCompleted), name: .initialSyncCompleted, object: nil)
-
-        NotificationCenter.default.addObserver(self, selector: #selector(shouldRefreshDay), name: .shouldRefreshDay, object: nil)
-
-    }
-    
-    @objc func shouldRefreshDay(notification: Notification) {
-        animatingMeal = true
-        self.reload()
-    }
-    
-    @objc func initialSyncCompleted(notification: Notification) {
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 10.0) {
-            cprint("📩 initialSyncCompleted → DayView")
-            self.animatingMeal = true
-            self.reload()
-//        }
-    }
-    
-    @objc func didSetBadgeWidths(notification: Notification) {
-        guard let userInfo = notification.userInfo as? [String: AnyObject],
-              let date = userInfo[Notification.Keys.date] as? Date,
-              date == self.date
-        else { return }
-        cprint("📩 didSetBadgeWidths → DayView")
-        reload()
-    }
-
-    @objc func didAddFoodItemToMeal(notification: Notification) {
-        cprint("📩 didAddFoodItemToMeal → DayView")
-        animatingMeal = true
-        reload()
-    }
-
-    @objc func didDeleteFoodItemFromMeal(notification: Notification) {
-        guard let userInfo = notification.userInfo as? [String: AnyObject],
-              let id = userInfo[Notification.Keys.uuid] as? UUID
-        else { return }
-        cprint("📩 didDeleteFoodItemFromMeal → DayView")
-        resetSortPositions(afterDeletingId: id)
-        animatingMeal = true
-        reload()
-    }
-    
-    @objc func didUpdateMealFoodItem(notification: Notification) {
-        guard let userInfo = notification.userInfo as? [String: AnyObject],
-              let updatedFoodItem = userInfo[Notification.Keys.foodItem] as? FoodItem
-        else { return }
-        cprint("📩 didDeleteFoodItemFromMeal → DayView")
-        resetSortPositions(afterUpdating: updatedFoodItem)
-        animatingMeal = true
-        reload()
-    }
-
-    func resetSortPositions(afterUpdating updatedFoodItem: FoodItem) {
-        resetSortPositions(updatedFoodItem: updatedFoodItem)
-    }
-
-    func resetSortPositions(afterDeletingId deletedFoodItemId: UUID) {
-        resetSortPositions(deletedFoodItemId: deletedFoodItemId)
-    }
-    
-    private func resetSortPositions(updatedFoodItem: FoodItem? = nil, deletedFoodItemId: UUID? = nil) {
-        let id = updatedFoodItem?.id ?? deletedFoodItemId
-        for meal in dayMeals {
-            var mealCopy = meal
-            guard let existingIndex = mealCopy.foodItems.firstIndex(where: { $0.id == id })
-            else { continue }
-
-            let before = mealCopy.foodItems
-            
-            print("-- Before setting updated item:")
-            for foodItem in mealCopy.foodItems {
-                print("    \(foodItem.sortPosition) \(foodItem.food.emoji) \(foodItem.food.name)")
-            }
-            
-            if let updatedFoodItem {
-                mealCopy.foodItems[existingIndex] = MealFoodItem(from: updatedFoodItem)
-            } else if let deletedFoodItemId {
-                mealCopy.foodItems.removeAll(where: { $0.id == deletedFoodItemId })
-            }
-            
-            print("-- Before sorting:")
-            for foodItem in mealCopy.foodItems {
-                print("    \(foodItem.sortPosition) \(foodItem.food.emoji) \(foodItem.food.name)")
-            }
-
-            mealCopy.foodItems.resetSortPositions(aroundFoodItemWithId: updatedFoodItem?.id)
-            mealCopy.foodItems.sort { $0.sortPosition < $1.sortPosition }
-
-            print("-- After sorting:")
-            for foodItem in mealCopy.foodItems {
-                print("    \(foodItem.sortPosition) \(foodItem.food.emoji) \(foodItem.food.name)")
-            }
-
-            for oldItem in before {
-                guard let newItem = mealCopy.foodItems.first(where: { $0.id == oldItem.id }) else {
-                    continue
-                }
-                if newItem.sortPosition != oldItem.sortPosition {
-                    do {
-                        print("-- Silently updating: \(newItem.sortPosition) \(newItem.food.emoji) \(newItem.food.name)")
-                        try DataManager.shared.silentlyUpdateSortPosition(for: newItem)
-                    } catch {
-                        cprint("Error updating sort position: \(error)")
-                    }
-                }
-            }
-            print(" ")
-        }
-    }
-    
-    @objc func didUpdateFoodItems(notification: Notification) {
-        cprint("📩 didUpdateFoodItems → DayView")
-        animatingMeal = true
-        reload()
-    }
-    
-    @objc func didUpdateMeal() {
-        cprint("📩 didUpdateMeal → DayView")
-        animatingMeal = true
-        reload()
-    }
-
-    @objc func didAddMeal() {
-        cprint("📩 didAddMeal → DayView, calling reload()")
-        animatingMeal = true
-        reload()
-        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
-            cprint("📩     (2s later) calling reload() again")
-            self.reload()
-        }
-    }
-
-    @objc func didDeleteMeal() {
-        cprint("📩 didDeleteMeal → DayView")
-        animatingMeal = true
-        reload()
-    }
-
-    func reload() {
-        load(for: date)
-    }
-    
-    func load(for date: Date) {
-        let day = DataManager.shared.day(for: date)
-        self.day = day
-        self.dayMeals = day?.meals ?? []
-        cprint("🧨 ----------")
-        cprint("🧨 DayView.load(for: \(date.calendarDayString)) — \(dayMeals.count) meals")
-        for meal in dayMeals {
-            cprint("🧨    Meal: \(meal.name) @ \(meal.timeString)")
-            for foodItem in meal.foodItems {
-                cprint("🧨        \(foodItem.sortPosition) \(foodItem.food.emoji) \(foodItem.food.name) - \(foodItem.badgeWidth)")
-            }
-        }
-        self.showingEmpty = dayMeals.isEmpty
-        cprint("🧨 ")
-    }
-
-    func dateChanged(_ newValue: Date) {
-        load(for: newValue)
-        DispatchQueue.main.asyncAfter(deadline: .now() + 0.5) {
-            self.previousDate = newValue
-        }
     }
 }
--- Sources/PrepMealsView/MetricsView.swift
+++ Sources/PrepMealsView/MetricsView.swift
@@ -9,56 +9,80 @@ struct MetricsView: View {
     @Environment(\.colorScheme) var colorScheme
     
     @Binding var date: Date
-
-    @State var energy: Double 
-    @State var carb: Double
-    @State var fat: Double
-    @State var protein: Double
-
+    
+    @State var nutrients: DayNutrients
+    
+    let shouldUpdateMetrics = NotificationCenter.default.publisher(for: .shouldUpdateMetrics)
+    let didAddFoodItem = NotificationCenter.default.publisher(for: .didAddFoodItemToMeal)
+    let didDeleteFoodItemFromMeal = NotificationCenter.default.publisher(for: .didDeleteFoodItemFromMeal)
+    let didUpdateMealFoodItem = NotificationCenter.default.publisher(for: .didUpdateMealFoodItem)
+    let didUpdateFoodItems = NotificationCenter.default.publisher(for: .didUpdateFoodItems)
+    let didDeleteMeal = NotificationCenter.default.publisher(for: .didDeleteMeal)
+    
     init(date: Binding<Date>) {
         _date = date
-        let energy: Double
-        let carb: Double
-        let fat: Double
-        let protein: Double
-        if let nutrients = DataManager.shared.nutrients(for: date.wrappedValue) {
-            energy = nutrients.energy
-            carb = nutrients.carb
-            fat = nutrients.fat
-            protein = nutrients.protein
-        } else {
-            energy = 0
-            carb = 0
-            fat = 0
-            protein = 0
-        }
-        _energy = State(initialValue: energy)
-        _carb = State(initialValue: carb)
-        _fat = State(initialValue: fat)
-        _protein = State(initialValue: protein)
+//        let nutrients: DayNutrients
+//        if let dayNutrients = DataManager.shared.nutrients(for: date.wrappedValue) {
+//            nutrients = dayNutrients
+//        } else {
+//            nutrients = .zero
+//        }
+        _nutrients = State(initialValue: .zero)
     }
     
     
     var body: some View {
-        GeometryReader { proxy in
-            ZStack {
-                Color(.systemGroupedBackground)
-                VStack {
-                    energyRow(proxy)
-                    macros(proxy)
+        Group {
+            GeometryReader { proxy in
+                ZStack {
+                    backgroundColor
+                    VStack {
+                        energyRow(proxy)
+                        macros(proxy)
+                    }
                 }
             }
-            .frame(height: 150)
         }
+        .frame(height: 150)
+        .onAppear(perform: loadData)
+        .onReceive(shouldUpdateMetrics, perform: update)
+        .onReceive(didAddFoodItem, perform: update)
+        .onReceive(didDeleteFoodItemFromMeal, perform: update)
+        .onReceive(didUpdateMealFoodItem, perform: update)
+        .onReceive(didUpdateFoodItems, perform: update)
+        .onReceive(didDeleteMeal, perform: update)
+    }
+    
+    func loadData() {
+        Task {
+            let dayNutrients = try await DataManager.shared.nutrients(for: date)
+            withAnimation {
+                self.nutrients = dayNutrients
+            }
+        }
+    }
+    
+    func update(_ notification: Notification) {
+        loadData()
+    }
+    
+    var backgroundColor: Color {
+        colorScheme == .light ? Color(.systemGroupedBackground) : Color(hex: "191919")
     }
     
     func energyRow(_ proxy: GeometryProxy) -> some View {
         VStack(spacing: 4) {
             HStack {
-                Text("\(energy.formattedEnergy) kcal")
-                    .font(.system(.title3, design: .rounded, weight: .semibold))
+                Color.clear
+                    .animatedEnergyValue(value: nutrients.energy)
+//                Text("\(nutrients.energy.formattedEnergy) kcal")
+//                    .font(.system(.title3, design: .rounded, weight: .semibold))
             }
-            FoodBadge(c: carb, f: fat, p: protein, width: .constant(proxy.size.width)
+            FoodBadge(
+                c: nutrients.carb,
+                f: nutrients.fat,
+                p: nutrients.protein,
+                width: .constant(proxy.size.width)
             )
         }
     }
@@ -71,15 +95,9 @@ struct MetricsView: View {
             return max(width, 0)
         }
         
-        func value(for macro: Macro) -> Double {
-            switch macro {
-            case .carb:
-                return carb
-            case .fat:
-                return fat
-            case .protein:
-                return protein
-            }
+        var backgroundColor: Color {
+            colorScheme == .light ? .white : Color(hex: "232323")
+            //            Color(.tertiarySystemBackground)
         }
         
         return HStack(spacing: spacing) {
@@ -95,14 +113,9 @@ struct MetricsView: View {
                         Spacer()
                     }
                     HStack {
-                        HStack(alignment: .firstTextBaseline, spacing: 2) {
-                            Spacer()
-                            Text(value(for: macro).formattedMacro)
-                                .font(.system(.title2, design: .rounded, weight: .medium))
-                            Text("g")
-                                .font(.system(.callout, design: .rounded, weight: .medium))
-                                .foregroundColor(Color(.secondaryLabel))
-                        }
+                        Spacer()
+                        Color.clear
+                            .animatedMacroValue(value: nutrients.value(for: macro))
                     }
                 }
                 .padding(.vertical, 5)
@@ -110,13 +123,98 @@ struct MetricsView: View {
                 .frame(width: macroWidth)
                 .background(
                     RoundedRectangle(cornerRadius: 10, style: .continuous)
-                        .fill(Color(.systemBackground))
+                        .fill(backgroundColor)
                 )
             }
         }
     }
 }
 
+struct AnimatableMacroValue: AnimatableModifier {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @State var size: CGSize = .zero
+    
+    var value: Double
+    
+    var animatableData: Double {
+        get { value }
+        set { value = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: size.width, height: size.height)
+            .overlay(
+                animatedLabel
+                    .readSize { size in
+                        self.size = size
+                    }
+            )
+    }
+    
+    var animatedLabel: some View {
+        HStack(alignment: .firstTextBaseline, spacing: 2) {
+            Text(value.formattedMacro)
+                .font(.system(.title2, design: .rounded, weight: .medium))
+            Text("g")
+                .font(.system(.callout, design: .rounded, weight: .medium))
+                .foregroundColor(Color(.secondaryLabel))
+        }
+        .frame(maxWidth: .infinity)
+        .multilineTextAlignment(.trailing)
+        .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+extension View {
+    func animatedMacroValue(value: Double) -> some View {
+        modifier(AnimatableMacroValue(value: value))
+    }
+}
+
+struct AnimatableEnergyValue: AnimatableModifier {
+    
+    @Environment(\.colorScheme) var colorScheme
+    @State var size: CGSize = .zero
+    
+    var value: Double
+    
+    var animatableData: Double {
+        get { value }
+        set { value = newValue }
+    }
+    
+    func body(content: Content) -> some View {
+        content
+            .frame(width: size.width, height: size.height)
+            .overlay(
+                animatedLabel
+                    .readSize { size in
+                        self.size = size
+                    }
+            )
+    }
+    
+    var animatedLabel: some View {
+        HStack(alignment: .firstTextBaseline, spacing: 2) {
+            Text(value.formattedEnergy)
+                .font(.system(.title, design: .rounded, weight: .semibold))
+            Text("kcal")
+                .font(.system(.title3, design: .rounded, weight: .regular))
+                .foregroundColor(Color(.secondaryLabel))
+        }
+        .frame(maxWidth: .infinity)
+        .multilineTextAlignment(.trailing)
+        .fixedSize(horizontal: true, vertical: false)
+    }
+}
+
+extension View {
+    func animatedEnergyValue(value: Double) -> some View {
+        modifier(AnimatableEnergyValue(value: value))
+    }
+}
 extension Macro {
     var abbreviatedDescription: String {
         switch self {
